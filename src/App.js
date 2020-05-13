6eacafd6193729a53db9629c2f3553ac89936a23
rk2609 <accounts@wanderon.in>
2020-05-13 13:06:56 +0530

    sign up with email and password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from "./pages/shop/shop";
 import SignInPage from "./pages/signin/signin";
 import ContactPage from "./pages/contact/contact";
 import Header from "./components/header/header";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -20,8 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(async (snapShot) => {
+          await this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./signin.scss";
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignInPage = () => (
-  <div className="sign-in">
+  <div className="sign-in-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
