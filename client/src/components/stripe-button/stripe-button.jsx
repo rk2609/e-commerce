f257eb45daf43b624df311f446fc93e91a0ab402
rk2609 <accounts@wanderon.in>
2020-05-17 01:10:29 +0530

    added backend

---
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckOutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_3k4BocSjC172jgc94FUJbZ3D00G1TdOzJi";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => alert("Payment successful"))
+      .catch((err) => {
+        console.log("Payment error: ", err);
+        alert(
+          "There was an issue with the payment. Please make sure you use the provided credit card"
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-Commerce"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is ₹${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckOutButton;
